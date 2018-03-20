8a97d76ecfc89eec31f93ee1f84288b3a6f29583
David Cano <david.cano@alfresco.com>
2018-03-20 17:07:07 +0000

    Smaller grid for tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,9 +9,9 @@ import { Task } from '../task';
 export class TasksComponent implements OnInit {
 
   tasks :Task[] = [
-    new Task('Task 1', 'David', 3, 1, 'This is de description for the new task 1', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date()),
-    new Task('Task 2', 'David', 3, 1, 'This is de description for the new task 2', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date()),
-    new Task('Task 3', 'David', 3, 1, 'This is de description for the new task 3', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date())
+    new Task('Task 1', 'David', 3, 1, 'This is the description for the new task 1', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date()),
+    new Task('Task 2', 'David', 3, 1, 'This is the description for the new task 2', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date()),
+    new Task('Task 3', 'David', 3, 1, 'This is the description for the new task 3', 'http://icons.iconarchive.com/icons/igh0zt/ios7-style-metro-ui/512/MetroUI-Other-Task-icon.png', new Date())
   ];
 
   constructor() { }
